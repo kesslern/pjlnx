20949776a5dbdcce7b3b4c20fd30530e7d2ba12d
Nathan Kessler <nathant93@gmail.com>
2019-09-18 19:48:28 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const glob = require("glob")
+const IRC = require("irc-framework")
+
+glob("./plugins/*.js")
+
+console.log("main")
+
+var bot = new IRC.Client();
+bot.connect({
+  host: 'irc.freenode.net',
+  port: 6667,
+  nick: 'pjlnx'
+});
+
+bot.on('message', function(event) {
+  if (event.message.indexOf('hello') === 0) {
+    event.reply('Hi!');
+  }
+  
+  if (event.message.match(/^!join /)) {
+    var to_join = event.message.split(' ')[1];
+    event.reply('Joining ' + to_join + '..');
+    bot.join(to_join);
+  }
+});
+
+
+// Or a quicker to match messages...
+bot.matchMessage(/^hi/, function(event) {
+  event.reply('hello there!');
+});
