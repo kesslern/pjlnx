952264c23dc2c5cd8ae8f173661fa017d2bec04c
Nathan Kessler <nathant93@gmail.com>
2019-09-18 20:32:17 -0400

    Example module export

--- main.js
+++ main.js
@@ -5,24 +5,32 @@ const plugins = require('require-all')({
   recursive: true
 });
 
+plugins.test.test1()
+
 console.log("main")
 
 var bot = new IRC.Client();
 bot.connect({
   host: 'irc.freenode.net',
   port: 6667,
-  nick: 'pjlnx'
-});
+  nick: 'pjlnx',
+  encoding: 'utf8',
+  auto_reconnect: true,
+  auto_reconnect_wait: 4000,
+  auto_reconnect_max_retries: 3,
+  ping_interval: 30,
+  ping_timeout: 120,
+})
 
 bot.on('message', function(event) {
   if (event.message.indexOf('hello') === 0) {
-    event.reply('Hi!');
+    event.reply('Hi!')
   }
   
   if (event.message.match(/^!join /)) {
-    var to_join = event.message.split(' ')[1];
-    event.reply('Joining ' + to_join + '..');
-    bot.join(to_join);
+    var to_join = event.message.split(' ')[1]
+    event.reply('Joining ' + to_join + '..')
+    bot.join(to_join)
   }
 });
 
--- plugins/test.js
+++ plugins/test.js
@@ -1,2 +1,7 @@
 console.log("test")
 
+const test1 = () => {
+  console.log('test1')
+}
+
+module.exports = { test1 }
