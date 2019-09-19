701ec7bcf65b7b61a08bc69e1841074b6750b03e
kesslern <nathant93@gmail.com>
2019-09-18 23:02:49 -0400

    Remove unused code

--- main.js
+++ main.js
@@ -26,22 +26,12 @@ bot.connect({
 })
 
 bot.on('message', function(event) {
-  if (event.message.indexOf('hello') === 0) {
-    event.reply('Hi!')
-  }
-  
   if (event.message.match(/^!join /)) {
     var to_join = event.message.split(' ')[1]
     event.reply('Joining ' + to_join + '..')
     bot.join(to_join)
   }
-});
-
-
-// Or a quicker to match messages...
-bot.matchMessage(/^hi/, function(event) {
-  event.reply('hello there!');
-});
+})
 
 for (key in plugins) {
   const { plugin } = plugins[key]
