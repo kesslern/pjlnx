2d51387d3d306c3911e944ec7d54b75e9cf72ad7
kesslern <nathant93@gmail.com>
2019-09-18 21:52:33 -0400

    Add example plugin for http requests

---
+++ plugins/advice.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+
+const plugin = (bot) => {
+  bot.matchMessage(/^advice$/, async event => {
+    const response = await axios.get("https://api.adviceslip.com/advice")
+    event.reply(response.data.slip.advice)
+  })
+}
+
+module.exports = {
+  plugin
+}
