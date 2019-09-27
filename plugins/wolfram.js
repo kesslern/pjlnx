48a8c3d14a20c84eceb2698400587e63861a03a2
Nathan Kessler <nathant93@gmail.com>
2019-09-26 20:51:40 -0400

    Add wolfram alpha plugin

---
+++ plugins/wolfram.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const parseString = require('xml2js').parseString
+
+const plugin = bot => {
+  const api = {
+    url: 'http://api.wolframalpha.com/v2/query',
+    key: bot.config.wolframAlpha,
+  }
+  
+  bot.matchCommand(/^(wa|wolfram) .+/, async event => {
+    const { command } = event
+    const input = command.slice(command.indexOf(" ") + 1)
+    const { data } = await axios.get(`${api.url}?appid=${api.key}&input=${input}`)
+
+    parseString(data, (err, xml) => {
+      const { pod: pods } = xml.queryresult
+      const result = pods
+            .filter(pod => pod['$'].primary)
+            .filter(pod => pod['$'].id !== 'Input')
+            .map(pod => pod.subpod.map(subpod => subpod.plaintext).flat())
+            .join('-')
+      event.reply(`Result: ${result} | Input: ${input}`)
+    })
+  })
+}
+
+module.exports = {
+  name: 'wolfram',
+  plugin,
+}
